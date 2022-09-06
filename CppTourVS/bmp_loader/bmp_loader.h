94c9e6e0766ee5a398f883638bfe7e9c28af5eab
AlexMercer <redownloader999@gmail.com>
2022-09-06 09:48:44 +0300

    bmp images converter

--- CppTourVS/bmp_loader/bmp_loader.h
+++ CppTourVS/bmp_loader/bmp_loader.h
@@ -1,16 +1,42 @@
+#pragma once
 #include <iostream>
-#include <fstream>
+#include <vector>
+#include <map>
+#include <memory>
 
+struct Bitmap {
+public:
+ Bitmap(unsigned int xp,
+        unsigned int yp,
+        unsigned char R,
+        unsigned char G,
+        unsigned char B)
+  : xp(xp), yp(yp), R(R), G(G), B(B) {
+ };
+ unsigned int xp;
+ unsigned int yp;
+ unsigned char R;
+ unsigned char G;
+ unsigned char B;
+};
 
+/*This class can load *.bpm  
+images and save them as array of pixels
+**/
+class BMP_Sprites_Hub {
+public:
+ BMP_Sprites_Hub() = default;
+ // std::map< std::string -> Filename , std::vector<Bitmap> -> Bitmap > Holds all bmp images
+ //std::unique_ptr<std::map<std::string, std::vector<Bitmap>>> *sprites_hub = nullptr;
 
-unsigned char* ReadBMP(char* filename) {
-  int i;
-
-  FILE* f = fopen_s(filename, "rb");
+ // Converts bpm file to vector<Bitmap> array
+ void  bmp_convert(std::string filename, std::vector<Bitmap>& bitmap) {
+  FILE* f;
+  fopen_s(&f, filename.c_str(), "r+");
 
   if (f == NULL)
-    throw "Argument Exception";
-
+   return;
+ 
   unsigned char info[54];
   fread(info, sizeof(unsigned char), 54, f);  // read the 54-byte header
 
@@ -18,25 +44,33 @@ unsigned char* ReadBMP(char* filename) {
   int width = *(int*)&info[18];
   int height = *(int*)&info[22];
 
-  std::cout << std::endl;
-  std::cout << "  Name: " << filename << std::endl;
-  std::cout << " Width: " << width << std::endl;
-  std::cout << "Height: " << height << std::endl;
+
 
   int row_padded = (width * 3 + 3) & (~3);
   unsigned char* data = new unsigned char[row_padded];
   unsigned char tmp;
 
-  for (int i = 0; i < height; i++) {
-    fread(data, sizeof(unsigned char), row_padded, f);
-    for (int j = 0; j < width * 3; j += 3) {
-      // Convert (B, G, R) to (R, G, B)
-      tmp = data[j];
-      data[j] = data[j + 2];
-      data[j + 2] = tmp;
-
-      std::cout << "R: " << (int)data[j] << " G: " << (int)data[j + 1]
-                << " B: " << (int)data[j + 2] << std::endl;
+  for (int i = 0; i < height; i++)
+  {
+   fread(data, sizeof(unsigned char), row_padded, f);
+   for (int j = 0; j < width * 3; j += 3)
+   {
+    // Convert (B, G, R) to (R, G, B)
+    tmp = data[j];
+    data[j] = data[j + 2];
+    data[j + 2] = tmp;
+    if ((int)data[j] != 255 && (int)data[j + 1] != 255 &&
+        (int)data[j + 2] != 255)
+    {
+     Bitmap bm =
+      Bitmap(i, j / 3, (int)data[j], (int)data[j + 1], (int)data[j + 2]);
+     bitmap.push_back(bm);
     }
+   }
   }
-}
+
+ }
+
+};
+
+
