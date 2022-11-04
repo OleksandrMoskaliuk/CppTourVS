9b3dac2ff58e7cea948a5b1dc5deef2e0dc0f241
AlexMercer <redownloader999@gmail.com>
2022-11-04 10:23:30 +0200

    removed redundant code

--- CppTourVS/Multithreading/Future/my_future.cpp
+++ CppTourVS/Multithreading/Future/my_future.cpp
@@ -13,10 +13,9 @@ void future() {
 }
 
 
- void future2() {
-
+ void future_example() {
   std::future<int> future = std::async(std::launch::async, []() {
-   // std::this_thread::sleep_for(std::chrono::seconds(10));
+   std::this_thread::sleep_for(std::chrono::seconds(5));
    return 8;
   });
 
@@ -32,26 +31,7 @@ void future() {
    }
   }
   while (status != std::future_status::ready);
-  std::cout << "waiting...\n";
-  std::future_status status2;
-  do
-  {
-   switch (status = future.wait_for(std::chrono::seconds(1)), status)
-   {
-    case std::future_status::deferred:
-     std::cout << "deferred\n";
-     break;
-    case std::future_status::timeout:
-     std::cout << "timeout\n";
-     break;
-    case std::future_status::ready:
-     std::cout << "ready!\n";
-     break;
-   }
-  }
-  while (status != std::future_status::ready);
-
-  std::cout << "result is " << future.get() << '\n';
+  //   get result
   std::cout << "result is " << future.get() << '\n';
  }
 
