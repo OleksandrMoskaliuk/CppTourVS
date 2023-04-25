6dcb5308242923a8540fc50d085b55c96b8cb5d3
AlexMercer <redownloader999@gmail.com>
2023-04-25 11:58:59 +0300

    few template added

---
+++ DesignPatterns/Source/Interpreter/Interpreter.cpp
@@ -0,0 +1,5 @@
+// Design problem
+// Inflexible expression of input processing
+
+// Interpreter intent
+// Given a language, define  a representation for its grammar, along with interpreter that uses the representation to interpreter 
