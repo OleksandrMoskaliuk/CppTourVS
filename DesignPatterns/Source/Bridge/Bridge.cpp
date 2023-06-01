08c4b9beffa5f36216f070a56bd3913936502d04
AlexMercer <redownloader999@gmail.com>
2023-06-01 14:20:18 +0300

    Bridge pattern

--- DesignPatterns/Source/Bridge/Bridge.cpp
+++ DesignPatterns/Source/Bridge/Bridge.cpp
@@ -2,4 +2,53 @@
 // Minimizing impact of variability
 
 // Bridge intent
-// Separate an abstraction from its implementations so the two can vary independently  
+// Separate an abstraction from its implementations so the two can vary
+// independently
+
+/**
+ * The Implementation defines the interface for all implementation classes. It
+ * doesn't have to match the Abstraction's interface. In fact, the two
+ * interfaces can be entirely different. Typically the Implementation interface
+ * provides only primitive operations, while the Abstraction defines higher-
+ * level operations based on those primitives.
+ */
+
+#include "Bridge.h"
+
+namespace BridgePattern {
+
+/**
+ * Except for the initialization phase, where an Abstraction object gets linked
+ * with a specific Implementation object, the client code should only depend on
+ * the Abstraction class. This way the client code can support any abstraction-
+ * implementation combination.
+ */
+void ClientCode(const Abstraction& abstraction) {
+  // ...
+  std::cout << abstraction.Operation();
+  // ...
+}
+/**
+ * The client code should be able to work with any pre-configured abstraction-
+ * implementation combination.
+ */
+
+int BridgePattern() {
+  Implementation* implementation = new ConcreteImplementationA;
+  Abstraction* abstraction = new Abstraction(implementation);
+  ClientCode(*abstraction);
+  std::cout << std::endl;
+  delete implementation;
+  delete abstraction;
+
+  implementation = new ConcreteImplementationB;
+  abstraction = new ExtendedAbstraction(implementation);
+  ClientCode(*abstraction);
+
+  delete implementation;
+  delete abstraction;
+
+  return 0;
+}
+
+}  // namespace BridgePattern
