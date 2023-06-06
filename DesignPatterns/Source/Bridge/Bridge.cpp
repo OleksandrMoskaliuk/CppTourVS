a5e0e4479df6a7fd27995bcba82493d4b3e83619
AlexMercer <redownloader999@gmail.com>
2023-06-06 12:45:27 +0300

    Moved class implementation explanation to the current header where class located

--- DesignPatterns/Source/Bridge/Bridge.cpp
+++ DesignPatterns/Source/Bridge/Bridge.cpp
@@ -5,14 +5,6 @@
 // Separate an abstraction from its implementations so the two can vary
 // independently
 
-/**
- * The Implementation defines the interface for all implementation classes. It
- * doesn't have to match the Abstraction's interface. In fact, the two
- * interfaces can be entirely different. Typically the Implementation interface
- * provides only primitive operations, while the Abstraction defines higher-
- * level operations based on those primitives.
- */
-
 #include "Bridge.h"
 
 namespace BridgePattern {
--- DesignPatterns/Source/Bridge/Bridge.h
+++ DesignPatterns/Source/Bridge/Bridge.h
@@ -5,6 +5,14 @@ namespace BridgePattern {
 
 int BridgePattern();
 
+
+/**
+ * The Implementation defines the interface for all implementation classes. It
+ * doesn't have to match the Abstraction's interface. In fact, the two
+ * interfaces can be entirely different. Typically the Implementation interface
+ * provides only primitive operations, while the Abstraction defines higher-
+ * level operations based on those primitives.
+ */
 class Implementation {
  public:
   virtual ~Implementation() {}
